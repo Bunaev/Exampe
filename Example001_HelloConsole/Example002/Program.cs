94cd42736f735e255cd60b765e6a84eeaad434b3
Konstantin <Bunaevkonstantin@bk.ru>
2023-02-23 19:47:50 +0400

    Create gitignor2

---
+++ Example001_HelloConsole/Example002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Маша - пять рублей и наша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
